0f9e6cb9711c908777cd0f7078445b3370520da3
irfankhan <er.irfan2798@gmail.com>
2023-12-07 17:09:29 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [text, settext] = useState("")
   return (
     <div>
-        {/* Do not remove the main div */}
+      <label htmlFor="name">Enter your name:</label>
+       <input type="text" id="name" onChange={e=>settext(e.target.value)}></input>
+       <p>{text}</p>
     </div>
   )
 }
 
 export default App
+ 
